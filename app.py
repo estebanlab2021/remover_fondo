766126f5580aae38f8ef72a0d317e9c4f09c01e7
Esteban Arroyo <79587959+estebanlab2021@users.noreply.github.com>
2025-07-19 18:49:03 -0400

    Se ha subido archivos principales

---
+++ app.py
@@ -0,0 +1,30 @@
+#import io
+from flask import Flask, request
+from flask_cors import CORS  # Importa la extensión CORS
+from rembg import remove
+
+app = Flask(__name__)
+
+CORS(app)  # Habilita CORS en toda la aplicación
+
+@app.route('/remover-fondo', methods=['POST', 'OPTIONS'])
+def remover_fondo():
+    if 'image' not in request.files:
+        return "No se ha subido una imagen", 400
+
+    image_file = request.files['image']
+    input_image = image_file.read()
+
+    # Procesa la imagen usando la librería rembg
+    output_image = remove(input_image)
+
+    if not output_image:
+        return "La imagen no pudo ser procesada por el servidor.", 500
+    
+    # Retorna los datos binarios directamente, sin usar send_file
+    return output_image, 200, {'Content-Type': 'image/png'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# python app.py
